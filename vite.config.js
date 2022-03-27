9d62a6675864295c38e99ac822b29e5915a6f363
rayc2045 <>
2022-03-27 19:41:10 +0800

    Add pug

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+import pugPlugin from 'vite-plugin-pug';
 import babel from 'vite-plugin-babel';
 
 export default defineConfig({
   plugins: [
+    pugPlugin(),
     babel(),
     // babel({
     //   babelConfig: {
@@ -12,4 +16,12 @@ export default defineConfig({
     //   },
     // }),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about/index.html'),
+      },
+    },
+  },
 });
